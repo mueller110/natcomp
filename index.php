a740bab0b9f3750405760e733ac099c80f4ceafd
Christian Andreas Mueller <cmueller@cosy.sbg.ac.at>
2014-05-14 11:16:33 +0200

    m

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    	<meta charset="utf-8">
+    	<title>Natural Computation</title>
+	<link rel="stylesheet" href="style.css">
+  </head>
+	
+
+
+  <body>
+	<h1>SIMMA - Design and implementation of a robot task demonstrating the effect of neuromodulators</h1>
+		<h2>members</h2>
+				
+		<table>	
+			<tr>				
+				<td>Tobias Auinger</td>
+				<td><a href="http://www.google.com/recaptcha/mailhide/d?k=01jOhY3pLf5Orgp4AJD5fhJw==&amp;c=2ncVSdtmilApo7dgEbirlSgReo3VDUasdHEYjpjS5Rs=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501jOhY3pLf5Orgp4AJD5fhJw\75\75\46c\0752ncVSdtmilApo7dgEbirlSgReo3VDUasdHEYjpjS5Rs\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Reveal e-mail address</a></td>
+			</tr>
+			<tr>							
+				<td>Christian Müller</td>
+				<td><a href="http://www.google.com/recaptcha/mailhide/d?k=01jOhY3pLf5Orgp4AJD5fhJw==&amp;c=odEv9hAwwj5DAmKnkT3IoTSHbgoJI7W9gSMBQgHESEk=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501jOhY3pLf5Orgp4AJD5fhJw\75\75\46c\75odEv9hAwwj5DAmKnkT3IoTSHbgoJI7W9gSMBQgHESEk\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address"">Reveal e-mail address</a></td>
+			</tr>
+			<tr>
+				<td>Stefan Schwarz</td>
+				<td><a href="http://www.google.com/recaptcha/mailhide/d?k=01jOhY3pLf5Orgp4AJD5fhJw==&amp;c=pLDuYYBt8UhVS2c62MkdInNW-vkDvDgwgMsVmCWtB4A=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501jOhY3pLf5Orgp4AJD5fhJw\75\75\46c\75pLDuYYBt8UhVS2c62MkdInNW-vkDvDgwgMsVmCWtB4A\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Reveal e-mail address</a></td>
+			</tr>
+			<tr>							
+				<td>Andreas Pollhamer</td>
+				<td><a href="http://www.google.com/recaptcha/mailhide/d?k=01jOhY3pLf5Orgp4AJD5fhJw==&amp;c=BYsTOKLWiMj2Pshhw1aOHfT7h-p8BCcMso8IjILe51I=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501jOhY3pLf5Orgp4AJD5fhJw\75\75\46c\75BYsTOKLWiMj2Pshhw1aOHfT7h-p8BCcMso8IjILe51I\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Reveal e-mail address</a></td>
+			</tr>
+			</table>
+	<h2>the project</h2>
+	
+	<ol>
+		<h3>abstract</h3>
+		
+		<h3>milestones</h3>
+			<ol>
+			<li>2014/04/2: Research and task design <a href="Files/p1.pdf">(p1)</a><a href="Files/p1latex.pdf">(p1Latex)</a></li>
+			</ol>
+	<oll>
+
+
+			
+  </body>
+</html>
+
